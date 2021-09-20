0daea9a5cdaa3258382361e826e18c1870d1ae5f
Сергей Андреевич Стрельников <90750905+SergeyStrelnikov1@users.noreply.github.com>
2021-09-20 17:50:06 +0300

    Update Lab1_211_353_StrelnikovSergey.cpp

--- Lab1_211_353_StrelnikovSergey.cpp
+++ Lab1_211_353_StrelnikovSergey.cpp
@@ -1,20 +1,48 @@
-﻿// Lab1_211_353_StrelnikovSergey.cpp : Определяет точку входа для приложения.
 #include <iostream>
 #include <string>
+#include <locale.h>
 
 int main()
 {
-    std::cout << "Hello, world !" << std::endl;
-    std::cout << "double\t" << sizeof(double) << std::endl;
-    double temp_double;// объявление переменных (резервирование месата)
-    temp_double = 0b0111111111111111111111111111111111111111111111111111111111111111111;
-    std::cout << "Max double " << temp_double;
-    temp_double = 0b0100000000000000000000000000000000000000000000000000000000000000000;
-    std::cout << " Min double " << temp_double;
-    int temp_int = 0b0111111111111111111111111111111;
-    int m_temp_int = 0b0100000000000000000000000000000;
+    setlocale(LC_ALL, "Russiаn");
+    std::cout << "1." << std::endl;
+    std::cout << "Strelnikov Sergey Andreevich, 211-353" << std::endl;
+    std::cout << "2." << std::endl;
 
-
-
+    double d1 = 0b01111111111111111111111111111111;
+    std::cout << "Max double " << d1;
+    d1 = 0b010000000000000000000000000000000;
+    std::cout << " Min double " << d1;
+    std::cout << "  double " << sizeof(double) << std::endl;
+    std::cout << "\n3." << std::endl;
+    int num = 0;
+    std::cout << "\nВведите число: ";
+    std::cin >> num;
+    int n = num;
+    unsigned long long int i = 1, bin = 0;
+    while (n > 0) {
+        bin += (n % 2) * i;
+        n /= 2;
+        i *= 10;
+    }
+    std::cout << "\nВ бинарном виде " << bin << std::endl;
+    std::cout << "\nВ шестнадцатиричном виде " << std::hex << num << std::endl;
+    std::cout << "\nbool " << bool(num) << std::endl;
+    std::cout << "\ndouble " << double(num) << std::endl;
+    std::cout << "\nchar " << char(num) << std::endl;
+    std::cout << "\n4." << std::endl;
+    std::cout << "Введите коэффициенты a * x = b:";
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << double(a) << " * x = " << double(b) << std::endl;
+    std::cout << "x = " << double(b) << " / " << double(a) << std::endl;
+    std::cout << "x = " << double(b) / double(a) << std::endl;
+    std::cout << "Ответ : x = " << double(b) / double(a) << std::endl;
+    std::cout << "5." << std::endl;
+    std::cout << "Введите координаты отрезка на прямой: ";
+    int c = 0;
+    int d = 0;
+    std::cin >> c >> d;
+    std::cout << "Середина отрезка находится в точке с координатой " << (double(c) + double(d)) / 2 << std::endl;
     return 0;
 }
