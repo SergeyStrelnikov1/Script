2c693297dcb9a030570c26316ffeedeed1da8cf3
Сергей Андреевич Стрельников <90750905+SergeyStrelnikov1@users.noreply.github.com>
2022-01-12 02:22:00 +0300

    12.01.22

---
+++ 6.5/6.5/6.5.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+int main() 
+{
+    string surname;
+    int group;
+    list<string> listSurname;
+    list<int> listGroup;
+    cout << "Break: 0 0" << endl;
+    while (true) 
+    {
+        cin >> group >> surname;
+        if (group == 0 || surname == "0")
+        {
+            break;
+        }
+
+        listSurname.push_back(to_string(group) + " " + surname);
+        listGroup.push_back(group);
+    }
+    cout << endl;
+    listGroup.sort();
+    listGroup.unique();
+    for (auto group : listGroup) 
+    {
+        cout << group << ":" << endl;
+        for (auto surname : listSurname)
+        {
+            if (surname.find(to_string(group)) != string::npos)
+            {
+                cout << surname << endl;
+            }
+        }
+        cout << endl;
+    }
+}
---
+++ 6.6/6.6/6.6.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+int main() 
+{
+    string input;
+    cout << "Break: 1" << endl;
+    map<string, int> mp;
+
+    double count = 0;
+    while (cin >> input)
+    {
+        if (input == "1") 
+        {
+            break;
+        }
+        count++;
+        if (mp.find(input) == mp.end()) 
+        {
+            mp[input] = 1;
+        }
+        else
+        {
+            mp[input] += 1;
+        }
+    }
+
+    for (auto i : mp) 
+    {
+        cout << i.first << " " << (float(i.second / count) * 100) << "%" << endl;
+    }
+}
