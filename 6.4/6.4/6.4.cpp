bde588d1dfcd7c01488df8da4f3f2d05f85747ec
Сергей Андреевич Стрельников <90750905+SergeyStrelnikov1@users.noreply.github.com>
2022-01-11 11:36:19 +0300

    6.4

---
+++ 6.4/6.4/6.4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+int main() {
+    set<string> st;
+    string input = "";
+    cout << "Break: exit" << endl;
+    while (true) {
+        cout << "Login: " << endl;
+        cin >> input;
+        if (input == "exit") {
+            break;
+        }
+
+        if (st.empty() || st.find(input) == st.end()) {
+            cout << "Login True: " << input << endl << endl;
+            st.insert(input);
+        }
+        else {
+            cout << "Login False: " << input << endl << endl;
+        }
+    }
+}
