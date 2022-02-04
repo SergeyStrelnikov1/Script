91312f01fb2608bd25fac4ff780f7058bf62e77d
Sergey <strelnikoff.sergey2016@yandex.ru>
2022-02-04 12:53:40 +0300

    test

---
+++ exampass/exampass/exampass.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<fstream>
+using namespace std;
+
+
+
+int main()
+{
+
+	string path = "myFile.txt";
+
+	fstream fs;
+	fs.open(path, fstream::in | fstream::out | fstream::app);
+	if (fs.is_open())
+	{
+		cout << " Ошибка открытия файла" << endl;
+	}
+	else
+	{
+		cout << " Открытия файла" << endl;
+		cout<<
+	}
+	return 0;
+}
